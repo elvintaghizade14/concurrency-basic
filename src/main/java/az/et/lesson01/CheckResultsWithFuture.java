17d12c3a36cb8769d409966970d1ee6de9e61e4f
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-11-23 23:04:23 +0400

    checking result with future class example added

---
+++ src/main/java/az/et/lesson01/CheckResultsWithFuture.java
@@ -0,0 +1,23 @@
+package az.et.lesson01;
+
+import java.util.concurrent.*;
+
+public class CheckResultsWithFuture {
+    private static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService service = null;
+        try {
+            service = Executors.newSingleThreadExecutor();
+            Future<?> result = service.submit(() -> {
+                for (int i = 0; i < 500; i++) CheckResultsWithFuture.counter++;
+            });
+            result.get(10, TimeUnit.SECONDS);
+            System.out.println("Reached!");
+        } catch (TimeoutException e) {
+            System.out.println("Not reached in time");
+        } finally {
+            if (service != null) service.shutdown();
+        }
+    }
+}
