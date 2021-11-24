25ae99288024e52af93bee5437a852340a239324
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-11-24 23:19:22 +0400

    example of using Callable in AddData.java file

---
+++ src/main/java/az/et/lesson02/AddData.java
@@ -0,0 +1,19 @@
+package az.et.lesson02;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class AddData {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService service = null;
+        try {
+            service = Executors.newSingleThreadExecutor();
+            Future<Integer> result = service.submit(() -> 30 + 11);
+            System.out.println(result.get());
+        } finally {
+            if (service != null) service.shutdown();
+        }
+    }
+}
