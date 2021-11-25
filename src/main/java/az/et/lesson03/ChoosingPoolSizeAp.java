34d57781d4bc8b0f496b28903b0aaf4be263c3cd
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-11-25 21:24:43 +0400

    to choose pool size identifying processor count

---
+++ src/main/java/az/et/lesson03/ChoosingPoolSizeAp.java
@@ -0,0 +1,9 @@
+package az.et.lesson03;
+
+public class ChoosingPoolSizeAp {
+    public static void main(String[] args) {
+        System.out.println(
+                Runtime.getRuntime().availableProcessors()
+        );
+    }
+}
